376cd7c43f227622feaeab8fa49adf06acfe5f57
Jayden Rajpurkar <jayden.rajpurkar@marquette.edu>
2023-12-05 15:19:09 -0600

    created customer class

---
+++ CarRental/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental
+{
+    internal class Customer
+    {
+        public int CustomerID { get; set; }
+        public string DriversLicenseID { get; set; }
+        public string Birthdate { get; set; }
+
+
+
+        public Customer(int customerID, string driversLicenseID, string birthdate)
+        {
+            CustomerID = customerID;
+            DriversLicenseID = driversLicenseID;
+            Birthdate = birthdate;
+        }
+        public void DisplayCustomerInfo()
+        {
+            Console.WriteLine($"Customer ID: {CustomerID}");
+            Console.WriteLine($"Driver's License Number: {DriversLicenseID}");
+            Console.WriteLine($"Birthdate: {Birthdate}");
+        }
+
+    }
+}
