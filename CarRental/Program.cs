4496146eda7b5f60c883318c37cbff7837dcde2b
fiirestyx <haigo2003@gmail.com>
2023-12-10 16:41:18 -0600

    Sign Up Menu. Needs comments

--- CarRental/Program.cs
+++ CarRental/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void SignUpMenu()
         {
-            
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string LastName = Console.ReadLine();
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            var newCustomer = new Customer
+            {
+                FirstName = firstName,
+                LastName = LastName,
+                Username = username,
+                Password = password
+            };
+
+            customers.customers.Add(newCustomer);
+
+            Console.WriteLine("Profile created!");
+
         }
+
     }
 }
