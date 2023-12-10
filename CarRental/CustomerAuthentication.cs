c30e7513122353cae8542d6674b30849768e4d78
Jayden Rajpurkar <jayden.rajpurkar@marquette.edu>
2023-12-10 16:40:45 -0600

    customer authentication class created (Needs comments)

---
+++ CarRental/CustomerAuthentication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental;
+
+public class CustomerAuthentication
+{
+    public List<Customer> customers { get; set; }
+
+    public CustomerAuthentication()
+    {
+        customers = new List<Customer>();
+    }
+    public Customer Authenticate(string username, string password)
+    {
+        var c = customers.Where(o => (o.UserName == username) && (o.password == password));
+        if (c.Count() > 0)
+        {
+            return c.First();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
