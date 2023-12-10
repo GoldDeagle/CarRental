7653c7c6ddcad18b7ea410a521b9cb5ee9c80e1a
GoldDeagle <duncantoby66@gmail.com>
2023-12-10 16:16:12 -0600

    added comments to request log

--- CarRental/RequestLog.cs
+++ CarRental/RequestLog.cs
@@ -10,15 +10,19 @@ namespace CarRental
     public class RequestLog
     {
         public Customer customer { get; set; }
+        // customer property gets and obtains the associated information about customer
+
 
         public Vehicle vehicle { get; set; }
-
+        // vehicle property gets and obtains the associated information about customer
     }
 
     public RequestLog(Customer c, Vehicle v)
+        // public constructor 'c' and 'v' parameters intializing the info with vehicle and customer
     {
         customer = c;
         vehicle = v;
+        // sets the associated objects with the properties of customer and vehicle
     }
 
 }
